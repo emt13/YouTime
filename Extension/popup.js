942766770419978d9b42f0e18b936f88b7deafd6
Evan Thompson <evanmt13@gmail.com>
2016-03-28 16:25:27 -0400

    Added some documentation

--- Extension/popup.js
+++ Extension/popup.js
@@ -21,10 +21,12 @@ document.addEventListener('DOMContentLoaded', function() {
   var extractButton = document.getElementById('extractButton');
   extractButton.addEventListener('click', function(){
     var extractField = document.getElementById('extractField');
+    //gets the active tab
     chrome.tabs.query({
       active: true,
       lastFocusedWindow: true
     }, function(arr) {
+      //using this to set the text field to the url
       var tab = arr[0];
       var url = tab.url;
       extractField.value=tab.url;
