4efc0922a2ab4c110cee47bdabe88936046cea4c
Evan Thompson <evanmt13@gmail.com>
2016-03-29 14:18:35 -0400

    Set body size

--- Extension/popup.js
+++ Extension/popup.js
@@ -1,22 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var checkPageButton = document.getElementById('checkPage');
-  checkPageButton.addEventListener('click', function(){
-    chrome.tabs.getSelected(null, function(tab) {
-      d = document;
-
-      var f = d.createElement('form');
-      f.action = 'http://gtmetrix.com/analyze.html?bm';
-      f.method = 'post';
-      var i = d.createElement('input');
-      i.type = 'hidden';
-      i.name = 'url';
-      i.value = tab.url;
-      f.appendChild(i);
-      d.body.appendChild(f);
-      f.submit();
-    });
-  }, false);
-
   //extract button code. extracts url and puts in text field
   var extractButton = document.getElementById('extractButton');
   extractButton.addEventListener('click', function(){
