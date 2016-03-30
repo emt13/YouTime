0bcafe62de38f27249c4175d3e3e0ae93fc08fd1
Evan Thompson <evanmt13@gmail.com>
2016-03-30 17:51:04 -0400

    Made stuff look nicer

--- Extension/popup.js
+++ Extension/popup.js
@@ -1,6 +1,35 @@
+var TIME_DESC_WIDTH = 29;
+
+function setURL(document, field){
+  chrome.tabs.query({
+    active: true,
+    lastFocusedWindow: true
+  }, function(arr) {
+    var tab = arr[0];
+    field.value = tab.url;
+  });
+}
+
+function descriptionSizing(document){
+  var timeField = document.getElementById('timeField');
+  var descField = document.getElementById('descField');
+  var elem = timeField.value;
+  timeField.size = timeField.value.length;
+  descField.size = TIME_DESC_WIDTH - timeField.size;
+  descField.value = descField.size + ' - ' + timeField.size;
+}
+
 document.addEventListener('DOMContentLoaded', function() {
+  descriptionSizing(document);
+  var extractField = document.getElementById('extractField');
+  setURL(document, extractField);
+
+
+  //var extractField = document.getElementById('extractField');
+  //extractField.value = getURL();
+
   //extract button code. extracts url and puts in text field
-  var extractButton = document.getElementById('extractButton');
+  /*var extractButton = document.getElementById('extractButton');
   extractButton.addEventListener('click', function(){
     var extractField = document.getElementById('extractField');
     //gets the active tab
@@ -33,5 +62,5 @@ document.addEventListener('DOMContentLoaded', function() {
     }else{
       editButton.value = 'Edit';
     }
-  }, false);
+  }, false);*/
 }, false);
