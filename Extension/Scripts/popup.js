0c5acb3ddafb2e869a9a1ef4af4f27b722dea8dd
Alexander Soloviev <alexander.serge.soloviev@gmail.com>
2016-04-06 19:11:22 -0400

    Added necessary comments and changed CSS

--- Extension/Scripts/popup.js
+++ Extension/Scripts/popup.js
@@ -1,30 +1,40 @@
-console.log("popup.js seen");
+/**
+ * This is the script injected into popup.html
+ * via a dynamic <script> tag. It serves as
+ * the main entry point to the extension logic.
+ * It does not have access to any of the
+ * chrome.* APIs.
+ *
+ * @author YouTime
+ * @version 1.0
+ * @since 2016-04-01
+ */
 
-//ping YouTube with a pause message
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+// Ping YouTube with a pause message, YTpauseVideo
+chrome.tabs.query( {active: true, currentWindow: true}, function(tabs) {
   console.log("sending pause");
-  //send the active tab a message trying to pause the video
-  chrome.tabs.sendMessage(tabs[0].id, {action: "YTpauseVideo"}, function(response) {
+  // Send the active tab a message trying to pause the video
+  chrome.tabs.sendMessage( tabs[0].id, {action: "YTpauseVideo"}, function(response) {
     console.log(response);
   });
 });
 
-//ping YouTube with the YTgetTitle message
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  //send the active tab a message asking for the title of the video
-  chrome.tabs.sendMessage(tabs[0].id, {action: "YTgetTitle"}, function(response) {
+// Ping YouTube with the title message, YTgetTitle
+chrome.tabs.query( {active: true, currentWindow: true}, function(tabs) {
+  // Send the active tab a message asking for the title of the video
+  chrome.tabs.sendMessage( tabs[0].id, {action: "YTgetTitle"}, function(response) {
     console.log(response);
-    //set the value in the popup.html
-    document.getElementById('nameField').value = response;
+    // Set the value in the popup.html
+    document.getElementById( "nameField" ).value = response;
   });
 });
 
-//ping YouTube with the YTgetTime message
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  //send the active tab a message asking for the title of the video
-  chrome.tabs.sendMessage(tabs[0].id, {action: "YTgetTime"}, function(response) {
+// Ping YouTube with the time message, YTgetTime
+chrome.tabs.query( {active: true, currentWindow: true}, function(tabs) {
+  // Send the active tab a message asking for the title of the video
+  chrome.tabs.sendMessage( tabs[0].id, {action: "YTgetTime"}, function(response) {
     console.log(response);
-    //set the value in the popup.html
-    document.getElementById('descriptionField').value = response;
+    // Set the value in the popup.html
+    document.getElementById( "descriptionField" ).value = response;
   });
 });
