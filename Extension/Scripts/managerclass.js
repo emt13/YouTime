6857fdd2a878e7741c408b89fd415ac2769f0561
Tom Veneziano <Tom Veneziano>
2016-04-20 19:23:07 -0400

    IT WORKS!!!

--- Extension/Scripts/managerclass.js
+++ Extension/Scripts/managerclass.js
@@ -28,15 +28,15 @@ YTManager.prototype.populatePage = function(timemarks){
       var a = document.createElement("a");
 
       //sets the href to the youtube link
-      a.setAttribute("href", mark[1]);
-      var hyperlink = mark[3] + " - " + mark[2];
+      a.setAttribute("href", mark['url']);
+      var hyperlink = mark['desc'] + " - " + mark['time'];
       a.appendChild(document.createTextNode(hyperlink));
       li.appendChild(a);
 
       //creates the share button. Adds a listener that allows you to copy the link
       var shareButton = document.createElement("BUTTON");
       shareButton.appendChild(document.createTextNode("Share"));
-      shareButton.setAttribute("url", mark[1]);
+      shareButton.setAttribute("url", mark['url']);
       shareButton.addEventListener("click", function() {
         window.prompt("Ctrl + C to copy this link:", this.getAttribute("url"));
       }, false);
